3d9539e470f237827290c9f0aebf831e8afa46d3
mugdha273 <mugdhasharma0327@gmail.com>
2021-08-08 16:45:21 +0530

    login and signup frontend

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,4 @@
 
-console.log('hi')
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
@@ -10,3 +9,14 @@ menu.onclick = () => {
 
 }
 
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
